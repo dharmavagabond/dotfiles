b54be6f34f4a8bde9c2646e8f72c07254e9d9b73
Omar Figueroa <omar.figueroa@protonmail.com>
2019-07-16 15:41:09 -0500

    Use of nix and config update.

--- .hyper.js
+++ .hyper.js
@@ -133,7 +133,13 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["verminal"],
+  plugins: [
+    "verminal",
+    "hyper-statusline",
+    "hyper-tabs-enhanced",
+    "hyper-pane",
+    "hypercwd"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
--- .zprofile
+++ .zprofile
@@ -1,6 +1,5 @@
 . $HOME/.conf
 . $(brew --prefix nvm)/nvm.sh
-. `brew --prefix`/etc/profile.d/z.sh
 
 # Function to get help on zsh functions and builtins
 unalias run-help
