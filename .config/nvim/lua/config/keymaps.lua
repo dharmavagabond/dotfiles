8d8d849f752a7d1b766aa0780c4bdbaf19523a11
Dharma <omar.figueroa@protonmail.com>
2025-07-15 23:09:03 -0600

    refactor(nvim): updated the keybindings for moving lines, deleted the unused

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,22 +4,10 @@
 
 local map = vim.keymap.set
 
--- Resize window using <ctrl> arrow keys
-map("n", "<Nop>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<Nop>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<Nop>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<Nop>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
 -- Move Lines
-map("n", "<C-A-j>", ":m .+2<cr>==", { desc = "Move down", silent = true })
-map("v", "<C-A-j>", ":m '>+2<cr>gv=gv", { desc = "Move down", silent = true })
-map("i", "<C-A-j>", "<Esc>:m .+1<cr>==gi", { desc = "Move down", silent = true })
-map("n", "<C-A-Down>", ":m .+1<cr>==", { desc = "Move down", silent = true })
-map("v", "<C-A-Down>", ":m '>+2<cr>gv=gv", { desc = "Move down", silent = true })
-map("i", "<C-A-Down>", "<Esc>:m .+1<cr>==gi", { desc = "Move down", silent = true })
-map("n", "<C-A-k>", ":m .0<cr>==", { desc = "Move up", silent = true })
-map("v", "<C-A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up", silent = true })
-map("i", "<C-A-k>", "<Esc>:m .-2<cr>==gi", { desc = "Move up", silent = true })
-map("n", "<C-A-Up>", ":m .-2<cr>==", { desc = "Move up", silent = true })
-map("v", "<C-A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up", silent = true })
-map("i", "<C-A-Up>", "<Esc>:m .-2<cr>==gi", { desc = "Move up", silent = true })
+map("n", "<C-A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<C-A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<C-A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<C-A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<C-A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<C-A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
