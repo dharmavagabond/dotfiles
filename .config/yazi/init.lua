830b1b7cc2f73e135c9b34356ce9df6ae7ae9143
Dharma <omar.figueroa@protonmail.com>
2025-10-03 21:30:33 -0600

    feat(yazi): enhanced with plugins and switched to rose-pine

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -11,3 +11,6 @@ function Linemode:size_and_mtime()
 	local size = self._file:size()
 	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
+
+require("no-status"):setup()
+require("duckdb"):setup()
