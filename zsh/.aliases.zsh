70014a1591acdcad618a2a456f58ff097371f7a5
Dharma <omar.figueroa@protonmail.com>
2025-10-29 17:35:55 -0600

    fix(zsh): fixed use of nvim in alias

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -4,4 +4,4 @@ alias l="eza -hal"
 alias tree="eza -hl --tree"
 alias vimdiff="nvim -d -R"
 alias sudo=doas
-alias sudoedit='doas env HOME="$HOME" XDG_CONFIG_HOME="$XDG_CONFIG_HOME" nvim'
+alias sudoedit='doas env HOME="$HOME" XDG_CONFIG_HOME="$XDG_CONFIG_HOME" mise x -- nvim'
