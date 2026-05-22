dd17ea22c3adcbe67136dd6fa084ebac62f9aad7
Dharma <omar.figueroa@protonmail.com>
2026-05-21 21:49:52 -0600

    refactor(hypr): dynamic sections require

--- .config/hypr/sections/init.lua
+++ .config/hypr/sections/init.lua
@@ -1,8 +1,10 @@
-require("hypr.sections.general")
-require("hypr.sections.decoration")
-require("hypr.sections.dwindle")
-require("hypr.sections.layout")
-require("hypr.sections.misc")
-require("hypr.sections.quirks")
-require("hypr.sections.render")
-require("hypr.sections.plugin")
+local handle = io.popen("ls -1 " .. os.getenv("XDG_CONFIG_HOME") .. "/hypr/sections/ 2>/dev/null")
+if handle then
+	for filename in handle:lines() do
+		if filename ~= "init.lua" and filename:match("%.lua$") then
+			require("hypr.sections." .. filename:gsub("%.lua$", ""))
+		end
+	end
+
+	handle:close()
+end
