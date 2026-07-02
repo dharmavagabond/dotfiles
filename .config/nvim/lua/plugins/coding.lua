4650a3319785a5a650ac388eab145c0842aa0cad
Dharma <github.wdaol@passmail.com>
2026-07-02 17:11:36 -0600

    feat(nvim): switched to dbee

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -135,4 +135,13 @@ return {
     version = "*",
     config = true,
   },
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      require("dbee").install()
+    end,
+  },
 }
