93762a1d288477fcc701cc6b98290ddcee0be41a
Dharma <github.wdaol@passmail.com>
2026-08-11 00:38:06 -0600

    chore(swayimg): updated depracated functions

--- .config/swayimg/init.lua
+++ .config/swayimg/init.lua
@@ -13,23 +13,23 @@ viewer.set_text("topright", {})
 viewer.set_text("bottomleft", {})
 
 viewer.on_key("h", function()
-	viewer.switch_image("prev")
+	viewer.open("prev")
 end)
 
 viewer.on_key("l", function()
-	viewer.switch_image("next")
+	viewer.open("next")
 end)
 
 viewer.on_key("j", function()
 	local pos = swayimg.get_mouse_pos()
-	local scale = viewer.get_scale()
+	local scale = viewer.scale
 	scale = scale - scale / 10
 	viewer.set_abs_scale(scale, pos.x, pos.y)
 end)
 
 viewer.on_key("k", function()
 	local pos = swayimg.get_mouse_pos()
-	local scale = viewer.get_scale()
+	local scale = viewer.scale
 	scale = scale + scale / 10
 	viewer.set_abs_scale(scale, pos.x, pos.y)
 end)
@@ -37,7 +37,7 @@ end)
 -- Gallery
 --
 
-gallery.set_border_color(0xffc4a7e7)
+gallery.border_color = 0xffc4a7e7
 
 gallery.set_text("topleft", {})
 
@@ -46,17 +46,17 @@ gallery.on_key("q", function()
 end)
 
 gallery.on_key("h", function()
-	gallery.switch_image("left")
+	gallery.select("left")
 end)
 
 gallery.on_key("l", function()
-	gallery.switch_image("right")
+	gallery.select("right")
 end)
 
 gallery.on_key("j", function()
-	gallery.switch_image("down")
+	gallery.select("down")
 end)
 
 gallery.on_key("k", function()
-	gallery.switch_image("up")
+	gallery.select("up")
 end)
