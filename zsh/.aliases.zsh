0da9df28a804d73ab5a1c2e245c33b5ba97b6977
Dharma <github.wdaol@passmail.com>
2026-08-27 16:35:26 -0600

    feat(zsh): more aliases (minus the yay one)

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -5,6 +5,7 @@ alias tree="eza -hl --tree"
 alias vimdiff="nvim -d -M"
 alias sudo=doas
 alias sudoedit=doasedit
-alias yay=paru
+alias visudo=vidoas
 alias pbpaste="wl-paste"
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
+alias kdiff="kitten diff"
